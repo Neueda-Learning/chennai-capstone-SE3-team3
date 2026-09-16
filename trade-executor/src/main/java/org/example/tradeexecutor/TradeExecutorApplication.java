ef737ee9b85638337da016408e68560639236b7d
LalithaPrakash <lalithaprakash324@gmail.com>
2026-09-16 17:30:14 +0000

    initial commit

---
+++ trade-executor/src/main/java/org/example/tradeexecutor/TradeExecutorApplication.java
@@ -0,0 +1,13 @@
+package org.example.tradeexecutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeExecutorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeExecutorApplication.class, args);
+    }
+
+}
